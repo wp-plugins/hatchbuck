7f86511a7f4325a76d2eb0fbeff76eb530d057e1
projectarmy <projectarmy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-25 23:09:14 +0000

    Added tag 1.1
    git-svn-id: https://plugins.svn.wordpress.org/hatchbuck/tags/1.1@1099741 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ hatchbuck.php
@@ -0,0 +1,68 @@
+<?php 
+/*
+Plugin Name: Hatchbuck
+Plugin URI: https://www.projectarmy.net
+Description: Hatchbuck for WordPress allows you to easily embed Hatchbuck forms inside any pages or posts using simple shortcodes. Plus, it makes it very easy to insert website tracking code on any page or post to enable tracking of these pages in Hatchbuck. ProjectArmy is an official Hatchbuck partner.      
+Version: 1.1
+Author: ProjectArmy
+Author URI: https://www.projectarmy.net
+Text Domain: Hatchbuck
+License: GPLv3 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+ob_start();
+
+// error_reporting(E_ALL);
+define('PLUGIN_NAME','Hatchbuck');
+define('HATCHBUCK_PLUGIN_FILE',__FILE__);
+define('HATCHBUCK_VERSION','1.1');
+define('HATCHBUCK_TABLE','hatchbuck_shortcode');
+
+require( dirname( __FILE__ ) . '/hatchbuck-functions.php' );
+
+require( dirname( __FILE__ ) . '/add_shortcode_tynimce.php' );
+
+require( dirname( __FILE__ ) . '/admin/addons/addons.php' );
+
+require( dirname( __FILE__ ) . '/admin/install.php' );
+
+require( dirname( __FILE__ ) . '/admin/menu.php' );
+
+require( dirname( __FILE__ ) . '/shortcode-handler.php' );
+
+require( dirname( __FILE__ ) . '/ajax-handler.php' );
+
+require( dirname( __FILE__ ) . '/admin/uninstall.php' );
+
+require( dirname( __FILE__ ) . '/direct_call.php' );
+
+require( dirname( __FILE__ ) . '/hatchbuck-admin-menu.php' );
+
+/*********** Get Started link on the Plugins page *******************/
+
+function hatchbuck_add_settings_link( $links ) {
+    $tutorial_link = '<a href="admin.php?page=hatchbuck-tutorial">' . __( 'Tutorial' ) . '</a>';
+	$settings_link = '<a href="admin.php?page=hatchbuck-manage">' . __( 'Get Started &raquo;' ) . '</a>';
+    array_push( $links, $tutorial_link, $settings_link );
+  	return $links;
+}
+$plugin = plugin_basename( __FILE__ );
+add_filter( "plugin_action_links_$plugin", 'hatchbuck_add_settings_link' );
+
+?>
