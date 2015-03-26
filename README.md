5b2495cea34a51ebbe9aef7fbfb0d08f3b1ce3d6
projectarmy <projectarmy@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-26 21:19:49 +0000

    Added version 1.2.1
    git-svn-id: https://plugins.svn.wordpress.org/hatchbuck/tags/1.2.1@1121755 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ README.md
@@ -0,0 +1,40 @@
+Hatchbuck for WordPress
+======================
+Hatchbuck for WordPress allows you to easily embed [Hatchbuck] forms inside any pages or posts using simple shortcodes. Plus, it makes it very easy to insert website tracking code on any page or post to enable tracking of these pages in Hatchbuck. [ProjectArmy] is an official [Hatchbuck] partner.
+
+Features:
+----
+  - Embed Hatchbuck forms with shortcodes anywhere in WordPress 
+  - Insert website tracking code on any page or post
+  - Admin toolbar shortcuts to your Hatchbuck app
+  - **NEW:** Addons - Form widget for sidebars (more coming soon!)
+
+Download Plugin:
+----
+  - You can download this plugin from the [WordPress.org repository](https://wordpress.org/plugins/hatchbuck/). 
+  - You can also visit your plugins page in WordPress, click on "Add New" and search for "Hatchbuck". Then simply install and activate the plugin.
+  
+Todo's
+----
+- Implement Hatchbuck API 
+- Gravity Form integration (waiting on API to support visitor tracking cookie)
+- Add more addons to extend functionality
+
+Support
+----
+- Free community support on [WordPress.org](https://wordpress.org/support/plugin/hatchbuck)
+- Premium support available to existing [ProjectArmy] clients
+
+Bugs
+----
+If you find any issues, please [let us know here].
+
+License
+----
+GNU v3 - See license file for details.
+
+**Effective Marketing. Sustainable Growth.**
+
+[ProjectArmy]:https://www.projectarmy.net
+[Hatchbuck]:http://www.hatchbuck.com/#_l_1t
+[let us know here]:https://github.com/ProjectArmy/Hatchbuck-for-WordPress/issues
---
+++ admin/header.php
@@ -0,0 +1,50 @@
+<?php
+wp_enqueue_script( 'itsec_modal', plugins_url(basename(dirname(dirname(__FILE__)))) . '/js/admin-modal.js', array( 'jquery' ), '', true );
+require( dirname( __FILE__ ) . '/modal_marketing_help.php' );
+wp_localize_script( 'itsec_modal', 'itsec_tooltip_text', array(
+      'nonce'    => '',
+      'messages' => (isset($messages))?$messages:'',
+      'title'    => 'Request Your FREE Consultation',
+    ));
+?>
+<?php if(isset($_GET['hb-mh'])): ?>
+<script type="text/javascript">
+  jQuery( document ).ready( function () {
+    jQuery('.hb-tab-market-help a').click();
+  });
+</script>
+<?php endif; ?>
+
+<div id="screen-meta-links">
+</div>
+
+<?php 
+  $pageName = 'Form';
+  $page = 'hatchbuck-manage';
+  if (isset($_GET)) {
+    if ($_GET['page'] == 'hatchbuck-settings') {
+      $pageName = 'Settings';
+      $page = 'hatchbuck-settings';
+    } elseif ($_GET['page'] == 'hatchbuck-addons') {
+      $pageName = 'Addons';
+      $page = 'hatchbuck-addons';
+    } elseif ($_GET['page'] == 'hatchbuck-help') {
+      $pageName = 'Help';
+      $page = 'hatchbuck-help';
+    } elseif ($_GET['page'] == 'hatchbuck-tutorial') {
+      $pageName = 'Tutorial';
+      $page = 'hatchbuck-tutorial';
+  }
+  }
+?>
+
+<div class="wrap">
+  <h2><?php echo PLUGIN_NAME; ?> - <?php echo $pageName; ?></h2>
+
+  <h2 class="nav-tab-wrapper">
+    <a href="?page=hatchbuck-manage" class="nav-tab <?php echo ($page == 'hatchbuck-manage')?'nav-tab-active':''; ?>">Forms</a>
+    <a href="?page=hatchbuck-settings" class="nav-tab <?php echo ($page == 'hatchbuck-settings')?'nav-tab-active':''; ?>">Settings</a>
+    <a href="?page=hatchbuck-addons" class="nav-tab <?php echo ($page == 'hatchbuck-addons')?'nav-tab-active':''; ?>">Addons</a>
+    <a href="?page=hatchbuck-help" class="nav-tab <?php echo ($page == 'hatchbuck-help')?'nav-tab-active':''; ?>">Help</a>
+    <a href="?page=hatchbuck-tutorial" class="nav-tab <?php echo ($page == 'hatchbuck-tutorial')?'nav-tab-active':''; ?>">Tutorial</a>
+  </h2>
